e2ec0f3b3f84d26e4754eb1086eafbf0d971c08e
BenyBoy84 <Blais_Benoit@hotmail.com>
2022-06-14 13:17:11 -0400

    Version 1.0

--- Scripts/Worker/Microsoft Windows/Endpoint Manager/RemoveMicrosoftOfficeOneNote.ps1
+++ Scripts/Worker/Microsoft Windows/Endpoint Manager/RemoveMicrosoftOfficeOneNote.ps1
@@ -1,24 +1,24 @@
 <#
 **********************************************************************************
-Script to remove AI Meeting Manager applications under user context
+Script to remove Microsoft Office OneNote applications under user context
 **********************************************************************************
 
 .SYNOPSIS
-Script to remove AI Meeting Manager applications under user context.
+Script to remove Microsoft Office OneNote applications under user context.
 
 Version 1.0 of this script.
 
 .DESCRIPTION
-This script is use to remove AI Meeting Manager applications under user context. 
+This script is use to remove Microsoft Office OneNote applications under user context. 
 
 This script accepts 2 parameters.
 -debug       This will generate display details informations in the Powershell window and a log file with the information related to the script execution.
 -output      This will generate an output file instead of displaying information in the Powershell window.
 
 .EXAMPLE
-./RemoveAIMeetingManager.ps1 
-./RemoveAIMeetingManager.ps1  -debug
-./RemoveAIMeetingManager.ps1  -output <path>
+./RemoveMicrosoftOfficeOneNote.ps1 
+./RemoveMicrosoftOfficeOneNote.ps1  -debug
+./RemoveMicrosoftOfficeOneNote.ps1  -output <path>
 
 .NOTES
 Author: Benoit Blais
@@ -31,14 +31,14 @@ https://github.com/benyboy84/Powershell
 Param(
     [String]$Application,
     [Switch]$Debug = $False,
-    [String]$Output = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RemoveAIMeetingManager.log"
+    [String]$Output = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RemoveMicrosoftOfficeOneNote.log"
 )
 
 #Default action when an error occured
 $ErrorActionPreference = "Stop"
 
 #Windows Application to remove
-$Applications = @("E046963F.AIMeetingManager")
+$Applications = @("Microsoft.Office.OneNote")
 
 # **********************************************************************************
 
--- Scripts/Worker/Microsoft Windows/Endpoint Manager/RemoveMicrosoftWindowsFeedbackHub.ps1
+++ Scripts/Worker/Microsoft Windows/Endpoint Manager/RemoveMicrosoftWindowsFeedbackHub.ps1
@@ -1,24 +1,24 @@
 <#
 **********************************************************************************
-Script to remove AI Meeting Manager applications under user context
+Script to remove Microsof Windows Feedback Hub applications under user context
 **********************************************************************************
 
 .SYNOPSIS
-Script to remove AI Meeting Manager applications under user context.
+Script to remove Microsof Windows Feedback Hub applications under user context.
 
 Version 1.0 of this script.
 
 .DESCRIPTION
-This script is use to remove AI Meeting Manager applications under user context. 
+This script is use to remove Microsof Windows Feedback Hub applications under user context. 
 
 This script accepts 2 parameters.
 -debug       This will generate display details informations in the Powershell window and a log file with the information related to the script execution.
 -output      This will generate an output file instead of displaying information in the Powershell window.
 
 .EXAMPLE
-./RemoveAIMeetingManager.ps1 
-./RemoveAIMeetingManager.ps1  -debug
-./RemoveAIMeetingManager.ps1  -output <path>
+./RemoveMicrosoftWindowsFeedbackHub.ps1 
+./RemoveMicrosoftWindowsFeedbackHub.ps1  -debug
+./RemoveMicrosoftWindowsFeedbackHub.ps1  -output <path>
 
 .NOTES
 Author: Benoit Blais
@@ -31,14 +31,14 @@ https://github.com/benyboy84/Powershell
 Param(
     [String]$Application,
     [Switch]$Debug = $False,
-    [String]$Output = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RemoveAIMeetingManager.log"
+    [String]$Output = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RemoveMicrosoftWindowsFeedbackHub.log"
 )
 
 #Default action when an error occured
 $ErrorActionPreference = "Stop"
 
 #Windows Application to remove
-$Applications = @("E046963F.AIMeetingManager")
+$Applications = @("Microsoft.WindowsFeedbackHub")
 
 # **********************************************************************************
 
